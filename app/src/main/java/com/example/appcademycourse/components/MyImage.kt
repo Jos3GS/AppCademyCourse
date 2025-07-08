a93e97295bb4d435239cf8b3668dd39e3d83c3b5
Jos3GS <gutierrez.sossa.j@gmail.com>
2025-07-08 17:55:04 -0500

    Sección de TextField y Button terminadas

---
+++ app/src/main/java/com/example/appcademycourse/components/MyImage.kt
@@ -0,0 +1,54 @@
+package com.example.appcademycourse.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.appcademycourse.R
+
+@Preview
+@Composable
+fun MyImage(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_foreground),
+            contentDescription = null,
+            modifier = Modifier.size(200.dp),
+            contentScale = ContentScale.Crop
+        )
+
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = null,
+            modifier = Modifier
+                .size(200.dp)
+                .clip(RoundedCornerShape(35))
+                .border(
+                    width = 5.dp,
+                    shape = RoundedCornerShape(35),
+                    brush = Brush.linearGradient(
+                        colors = listOf(
+                            Color.Red,
+                            Color.Cyan,
+                            Color.Yellow
+                        )
+                    )
+                ),
+            contentScale = ContentScale.Crop
+        )
+
+
+
+
+    }
+}
