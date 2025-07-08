11a4b777d0a8322684df51558e994d97e8cc73df
Jos3GS <gutierrez.sossa.j@gmail.com>
2025-07-08 18:15:51 -0500

    Sección de imagenes y icon terminadas

--- app/src/main/java/com/example/appcademycourse/components/MyImage.kt
+++ app/src/main/java/com/example/appcademycourse/components/MyImage.kt
@@ -1,10 +1,12 @@
 package com.example.appcademycourse.components
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -14,6 +16,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 import com.example.appcademycourse.R
 
 @Preview
@@ -46,9 +49,28 @@ fun MyImage(modifier: Modifier = Modifier) {
                 ),
             contentScale = ContentScale.Crop
         )
-
-
-
-
     }
+}
+
+@Preview
+@Composable
+fun MyNetworkImage(modifier: Modifier = Modifier) {
+    AsyncImage(
+        model = "https://www.concierto.cl/wp-content/uploads/2024/03/dua-lipa-confirma-los-detalles-de-su-proximo-album-radical-optimism-jpg.webp",
+        contentDescription = "Image from network",
+        modifier = Modifier.size(200.dp),
+        onError = {
+            Log.i("Image", "Ha ocurrido un error ${it.result}")
+        }
+    )
+}
+
+@Preview
+@Composable
+fun MyIcon(modifier: Modifier = Modifier) {
+    Icon(
+        painter = painterResource(R.drawable.ic_launcher_foreground),
+        contentDescription = null,
+        tint = Color.Red
+    )
 }
